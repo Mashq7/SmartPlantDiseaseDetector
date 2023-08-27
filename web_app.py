2e9d4980bd1d076093322ef04fa394b66df3c5f2
shaimaa-mk <sh.khwork2020@gmail.com>
2023-08-27 07:20:20 +0300

    Render Templates

--- web_app.py
+++ web_app.py
@@ -1,27 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1>Home Page</h1>"
+	return render_template('home.html')
 
 @app.route("/login")
 def login():
-	return 'Login URL'
+	return render_template('login.html')
 
 @app.route("/register")
 def register():
-	return 'Register URL'
+	return render_template('register.html')
 	
 @app.route("/project/")
 def project():
-	return 'Project URL'
+	return render_template('project.html')
 
 @app.route("/about")
 def about():
-	return 'about URL'
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
