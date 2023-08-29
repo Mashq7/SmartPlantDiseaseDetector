89591f990a3c4a96b34f4e36ad3483b21098abb8
shaimaa-mk <sh.khwork2020@gmail.com>
2023-08-29 17:11:50 +0300

    Add Fake Data to Users Table

--- web_app.py
+++ web_app.py
@@ -1,17 +1,50 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, jsonify
 from forms import RegistrationForm, LoginForm
 from models import spdd_db, User, Plant
+from models import spdd_ma, UserSchema, PlantSchema
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dc521c243424f382543b5bd4aa769b98'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 spdd_db.init_app(app)
-
+spdd_ma.init_app(app)
 
 @app.route("/create")
 def create():
 	spdd_db.create_all()  
 	return "Tables created!"
+
+@app.route("/create_user")
+def create_user():
+	# data = kwargs
+	# username = ''
+	# email = ''
+	# passwd = ''
+	# image_file = ''
+	# plants = []
+
+	# # input valdations
+	# if data[username]:
+	# 	username = data[username]
+	# if data[email]:
+	# 	email = data[email]
+	# if data[password]:
+	# 	passwd = data[password]
+	# if data[image_file]:
+	# 	image_file = data[image_file]
+
+	user = User(username='Adam', email='add_203@gmail.com', password='passwor2', image_file=None)
+	spdd_db.session.add(user)
+	spdd_db.session.commit()
+	return f'User {user.username} created!'
+
+@app.route("/users")
+def get_users():
+	users = User.query.all()
+	user_schema = UserSchema(many=True)
+	output = user_schema.dump(users)
+	return jsonify({'in': output})
 
 @app.route("/")
 @app.route("/home")
