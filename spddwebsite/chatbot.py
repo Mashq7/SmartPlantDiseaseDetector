4875982e74b9cb5aa9d146f57e2ea275ee0a28cd
shaimaa-mk <sh.khwork2020@gmail.com>
2023-08-31 22:28:08 +0300

    Update ChatGPT

--- spddwebsite/chatbot.py
+++ spddwebsite/chatbot.py
@@ -2,91 +2,17 @@ import openai
 
 openai.api_key = "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"
 
-class_name="Tomato__Target_Spot"
-
-# def get_completion(prompt, model="gpt-3.5-turbo"):
-#     messages = [{"role": "user", "content": str(prompt)}]
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages,
-#         temperature=1,  # this is the degree of randomness of the model's output
-#     )
-#     print( response.choices[0].message["content"])
-
-
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=temperature, # this is the degree of randomness of the model's output
+        temperature=temperature,
     )
-#     print(str(response.choices[0].message))
-    print(response.choices[0].message["content"])
+    return response.choices[0].message["content"]
 
 
-
-def collect_messages():
-    print("Welcome to the Plant Pathologist Chatbot!")
-    print("You can ask questions about plant diseases, including Tomato Target Spot.")
-    
-    context = [{'role': 'system', 'content': f"""
-        act as a Plant Pathologist and tell me more about {class_name}
-        ***********************************************
-        output: the output should take into consideration the following
-        - make the output 100 words at most
-        - use easy words to understand
-    """}]
-    
-    response = get_completion_from_messages(context)
-    print("Chatbot:", response)  # Display initial response
-    
-    while True:
-        prompt = input("You: ")
-        
-        if prompt.lower() in ["quit", "exit", "stop"]:
-            print("Chatbot: Conversation ended. Goodbye!")
-            break
-        
-        context.append({'role': 'user', 'content': prompt})
-        response = get_completion_from_messages(context)
-        print("Chatbot:", response)
-        
-    print("Chatbot session ended.")
-
-# Call the function to start the chatbot
-collect_messages()
-
-
-
-# context = [ {'role':'system', 'content':f"""
-
-#             act as a Plant Pathologist and tell me more about {class_name}
-#               ***********************************************
-#             output: the output should take in consideration the following
-#             - make the output 100 word at most
-#             - use easy words to understand
-
-
-# """} ]  # accumulate messages
-
-
-# def collect_messages():
-#     response = get_completion_from_messages(context)
-#     #crate a loop to keep the chatbot running
-#     while True:
-#         #enter the user input
-#         prompt = input("Enter your input: ")
-#         #inp.value = ''
-#         context.append({'role':'user', 'content':f"{prompt}"})
-#         response = get_completion_from_messages(context)
-#         context.append({'role':'assistant', 'content':f"{response}"})
-#         flag=input("Do you want to continue? (y/n)")
-#         if flag=='n':
-#             break
-#         else:
-#             collect_messages()
-#     return response
-
-        
-        
+def get_response(context_):
+    gptresponse = get_completion_from_messages(context_)
+    context_.append({'role': 'assistant', 'content': gptresponse})
+    return context_, gptresponse     
--- spddwebsite/routes.py
+++ spddwebsite/routes.py
@@ -1,25 +1,11 @@
-import torch
-from flask import render_template, flash, redirect, url_for, jsonify, request
+from flask import render_template, jsonify, request
 from spddwebsite import app
-import openai
 from PIL import Image
-from transformers import ResNetConfig, ResNetForImageClassification
-
-from torchvision import transforms
 from spddwebsite.spdd_model import classify_plant
-
-# Set your OpenAI API key
-openai.api_key = "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"
+from spddwebsite.chatbot import get_response
 
 
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-    )
-    return response.choices[0].message["content"]
-
+class_name = ''
 
 @app.route("/")
 @app.route("/index")
@@ -27,36 +13,33 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/disease_detection", methods=['GET','POST'])
+@app.route("/disease_detection")
 def project():
     return render_template('disease_detection.html')
 
 
 @app.route("/user_query", methods=['GET','POST'])
 def user_query():
-    context = [{'role': 'system', 'content': f"""
-        act as a Plant Pathologist and tell me more about {class_name}
-        ***********************************************
-        output: the output should take into consideration the following
-        - make the output 100 words at most
-        - use easy words to understand
-    """}]
-    if request.method == "GET":
-        gptresponse = get_completion_from_messages(context)
-        context.append({'role': 'assistant', 'content': gptresponse})
-        response = {"response" : gptresponse}
-        print(response)
+    if class_name:
+        context = [{'role': 'system', 'content': f"""
+            act as a Plant Pathologist and tell me more about {class_name}
+            ***********************************************
+            output: the output should take into consideration the following
+            - make the output 100 words at most
+            - use easy words to understand
+        """}]
+        if request.method == "GET":
+            context, response = get_response(context)
 
-    if request.method == "POST":
-        if request.json['prompt']:
-            prompt = request.json['prompt']
-            context.append({'role': 'user', 'content': prompt})
-            gptresponse = get_completion_from_messages(context)
-            context.append({'role': 'assistant', 'content': gptresponse})
-            response = { "response": gptresponse }
-            print(response)
-    
-    return  jsonify(response)
+        if request.method == "POST":
+            if request.json['prompt']:
+                prompt = request.json['prompt']
+                context.append({'role': 'user', 'content': prompt})
+                context, response = get_response(context)
+
+        return  jsonify({"response" : response})
+    else:
+        return  jsonify({"response" : "No Plant Found!"})
 
 
 @app.route("/team_member")
@@ -71,15 +54,14 @@ def resorce():
 
 @app.route("/datarespons", methods=['GET','POST'])
 def response(): 
+    global class_name
     image_file = request.files['image']
     outputs = classify_plant(image_file)
     class_name = outputs['name']
-    response={
-        "name":  outputs['name'],
-        "plant": outputs['plant'],
-        "healthy": outputs['healthy'],
-        "disease": outputs['disease'],
-        "plant_probability": outputs['plant_probability']
-    }
+    response = { "name":  outputs['name'],
+                "plant": outputs['plant'],
+                "healthy": outputs['healthy'],
+                "disease": outputs['disease'],
+                "plant_probability": outputs['plant_probability']}
     
     return  jsonify(response)
